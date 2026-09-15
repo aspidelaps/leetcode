808706ece788298e5f0f728f827cc780a930ee6f
Markar <b.g.markar@gmail.com>
2026-09-15 13:59:20 +0300

    Time: 1 ms (99.23%) | Memory: 42.4 MB (89.84%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,62 +1,10 @@
 class Solution {
 
     public static int mySqrt(int x) {
-
-        if (x == 0) {
-            return 0;
+        long r = x;
+        while (r*r > x) {
+            r = (r + x/r) / 2;
         }
-
-        if (x == 1) {
-            return 1;
-        }
-
-        if (x <= 100) {
-            if (x == 100) {
-                return 10;
-            }
-            return calculateKvadrat(1, 99, x);
-        }
-
-        if (x <= 10000) {
-            if (x == 10000) {
-                return 100;
-            }
-            return calculateKvadrat(10, 9999, x);
-        }
-
-        long right = Integer.MAX_VALUE;
-        long left = 101;
-        long mid = 0;
-        while ((right - left) > 1) {
-            mid = (left + right) / 2L;
-            long longMid = mid;
-            long kvadrat = longMid * longMid;
-            if (kvadrat == x) {
-                return (int) mid;
-            }
-            if (kvadrat > x) {
-                right = mid;
-            } else {
-                left = mid;
-            }
-        }
-        if (right*right > x) {
-            return (int) left;
-        } else return (int) right;
-    }
-
-    private static int calculateKvadrat(int start, int max, int x) {
-        int result = 0;
-        for (int i = start; i <= max; i++) {
-            int kvadrat = i * i;
-            if (kvadrat == x) {
-                return i;
-            }
-            if (kvadrat > x) {
-                return i - 1;
-            }
-            result = i;
-        }
-        return result;
+        return (int) r;
     }
 }
